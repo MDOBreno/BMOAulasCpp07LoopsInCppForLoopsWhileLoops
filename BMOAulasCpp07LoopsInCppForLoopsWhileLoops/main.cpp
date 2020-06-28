cffda2cd580ef4faa3986f4ceb19f37e17fcfb6c
Breno Medeiros de Oliveira <breno.medeiros@icloud.com>
2020-06-28 07:26:16 -0300

    Loops in C++ (for loops, while loops): [ https://youtu.be/_1AwR-un4Hk ]

--- BMOAulasCpp07LoopsInCppForLoopsWhileLoops/main.cpp
+++ BMOAulasCpp07LoopsInCppForLoopsWhileLoops/main.cpp
@@ -10,6 +10,71 @@
 
 int main(int argc, const char * argv[]) {
     // insert code here...
-    std::cout << "Hello, World!\n";
+    
+    std::cout << "====================== FOR ======================\n";
+    
+        for (int i=0; i<5; i++) {
+        std::cout << "Hello, World!\n";
+    }
+    
+    std::cout << "----------------\n";
+    
+    int i=0;
+    for (; i<5; ) {
+        std::cout << "Hello, World!\n";
+        i++;
+    }
+    
+    std::cout << "----------------\n";
+    
+    /*int*/ i=0;
+    bool condicao = true;
+    for (; condicao; ) {
+        std::cout << "Hello, World!\n";
+        i++;
+        if (!(i<5)) {
+            condicao = false;
+        }
+    }
+    
+    std::cout << "----------------\n";
+    
+    /*int*/ i=0;
+    for (; ; ) {
+        std::cout << "Hello, World!\n";
+        i++;
+        if (!(i<5)) {
+            break;
+        }
+    }
+    
+    std::cout << "====================== WHILE =====================\n";
+    
+    /*int*/ i=0;
+    while (i<5) {
+        std::cout << "Hello, World!\n";
+        i++;
+    }
+    
+    std::cout << "==================== DO WHILE ====================\n";
+    
+    /*int*/ i=0;
+    do {
+        std::cout << "Hello, World!\n";
+        i++;
+    } while (i<5);
+    
+    std::cout << "----------------\n";
+    
+    /*int*/ i=0;
+    /*bool*/ condicao = true;
+    while (condicao) {
+        std::cout << "Hello, World!\n";
+        i++;
+        if (!(i<5)) {
+            condicao = false; //Equivale a: break;
+        }
+    }
+    
     return 0;
 }
